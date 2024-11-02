383e2ca5161af71b1db4b999d2a2ddf0188668ef
GaziSohan <86655935+GaziSohan@users.noreply.github.com>
2024-11-03 01:44:19 +0600

    Add files via upload
    Evaldas Tylas is the CEO of Eastnetic, which is a IT Services and IT Consulting company located in Vilnius. He provided me with 2 datasets  - 
    1. car-golf-dataset.csv -  this is basically a catalog of all possible variations of models
    2. car-golf-info.csv -  is a dataset of actual classifieds data, actual cars basically
    
    He requested me to analyze the car classifieds from these datasets. As these classifieds are usually entered by a lot of different people, their quality is not consistent, some are accurate, some are not. But our goal is to have only the best and most accurate data. 
    
    The Task can be described as below (step by step) - 
    
    Prepare the data
    1. Come up with logic/algorithm to assign correct engine_type and category to car rows:
    1.1 engine_type is nicely defined in car-golf-info file (modification column. there’s also parsed_engine_type which should be accurate but needs double-checking to make sure) while car-golf-dataset file has title field that need to be processed to properly get accurate engine_type. i.e. title: Volkswagen Golf 1.5 TSI Comfortline R-Line Navi - engine_type is “TSI”, not “R”
    1.2 category value in car-golf-dataset have quite bad accuracy so need to use car-golf-info (submodel column has this info., also extracted to parsed_category) to improve category field.
    
    Clean the data
    2. Detect suspicious car rows. few examples:
    title: Volkswagen Golf 2.0 TDI DSG Klimatronic,Leder,Navi,1 Hand; transmission: Manual gearbox - DSG vs manual gearbox is contradicting so suspicious
    title: Volkswagen Golf VIII Lim. Style eHybrid DSG~LED~ACC~KAMERA~; fuel: Petrol - hybrid but fuel simply says petrol
    there’s no explicit power value but title has power
    power doesn’t match any possible variation so probably data was entered wrongly
    
    Visualize and make sense of data
    3. What key trends do you see in this database?
    Please communicate these in 2-4 charts with short (2-4 sentence) commentary
    
    The hint:
    Don't do it mechanically, you need to understand what that data means. Research the topic, ask questions if needed.
    Please share your thoughts/feedback/queries to Mr Evaldas regarding the task if you have any.

---
+++ SQL_Query_Car-golf-dataset.sql
@@ -0,0 +1,61 @@
+select
+	[source_id],[title],[make],[model],[power],[color],[capacity],[transmission],[fuel],[construction_year]
+    ,[price_net],[price_gross],[added],[mileage],[url],[category] as original_category,
+-- Logic/algorithm to assign correct engine_type to car rows in car-golf-dataset
+	case
+		when title like '%TSI%' then 'TSI'
+		when title like '%TDI%' then 'TDI'
+		when title like '%TDI-CR%' then 'TDI-CR'
+		when title like '%e-Golf%' then 'e-Golf'
+		when title like '%eTSI%' then 'eTSI'
+		when title like '%GTD%' then 'GTD'
+		when title like '%GTI%' then 'GTI'
+		when title COLLATE Latin1_General_BIN LIKE '% R %' then 'R'
+		when title like '%TSI eHybrid%' then 'TSI eHybrid'
+		when title like '%TSI GTE%' then 'TSI GTE'
+		when title like '%TSI Plug-In-Hybrid GTE%' then 'TSI Plug-In-Hybrid GTE'
+		else NULL
+	end as extracted_engine_type,
+-- Logic/algorithm to assign correct category to car rows in car-golf-dataset
+	case 
+			when category like '%Small Car%' or category like '%smallcar%' then 'Small Car'
+			when category like '%Van%' or category like '%Minibus%' then 'Van/Minibus'
+			when category like '%Estate%' then 'Estate Car'
+			when category like '%Cabrio%' or category like '%Roadster%' then 'Cabriolet/Roadster'
+			when category like '%Sports%' then 'Sports Car'
+			when category like '%Other%' then 'Other Car'
+			when category like '%Off%' then 'Off-road Vehicle'
+			when category like '%Saloon%' then 'Saloon'
+			when category like '%Limousine%' then 'Limousine'
+			else NULL
+		end as corrected_category,
+-- Flag for Transmission Mismatch
+	case
+		when transmission like '%auto%' and title like '%auto%' then 'Match'
+		when transmission like '%manu%' and title like '%manu%' then 'Match'
+		when transmission is NULL 
+			and title not like '%dsg%'
+			and title not like '%auto%'
+			and title not like '%manu%'
+		then 'Match'
+		else 'Mismatch, Suspicious'
+	end as transmission_mismatch,
+-- Flag for Fuel Mismatch
+	case
+		when cast(fuel as varchar(max)) like '%petrol%' and title like '%petrol%' then 'Match'
+		when cast(fuel as varchar(max)) like '%diesel%' and title like '%diesel%' then 'Match'
+		when cast(fuel as varchar(max)) like '%hybrid%' and title like '%hybrid%' then 'Match'
+		else 'Mismatch, Suspicious'
+	end as fuel_mismatch,
+-- Flag for Power Mismatch
+	case
+		when power = 0 and (title like '%kw%' or title like '%hp%' or title like '%ps%') then 'Mismatch in Power, Suspicious'
+		else 'power not missing'
+	end as 'missing_power',
+--Flag for Invalid Power Value (power does not match expected values)
+	case
+		when power <> 0 and (power < 50 or power > 500) then 'Invalid Power, Suspicious'
+		else 'power is valid'
+	end as 'invalid_power'
+from
+	[car-golf-dataset]
